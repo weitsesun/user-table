573804b6bba17d3da68090db19c96384443afa47
Wei-Tse Sun <wtse.sun@gmail.com>
2021-07-20 20:59:20 -0700

    format time

--- src/components/table/TableBody.js
+++ src/components/table/TableBody.js
@@ -1,33 +1,14 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import { get } from "lodash";
+import moment from "moment"
 
 export default function TableBody({ data, columns }) {
-  // const { data } = useSelector((state) => state.data);
-  // const parsedData = data.map(user => get(user, columns.map(c =), void 0))
-  // console.log(parsedData)
-  /**
-    const userColumns = [
-      {id: "firstName", value: "First Name"},
-      {id: "lastName", value: "Last Name"},
-      {id: "totalExpense", value: "Total Expense"},
-    ]
-   */
-  /**
-    const categoryColumns = [
-      {id: "category.something", value: "First Name"},
-      {id: "lastName", value: "Last Name"},
-      {id: "totalExpense", value: "Total Expense"},
-    ]
-   */
-  // function parseDataByColumnId(id) {
-
-  // }
-
   function renderCell(id, item) {
     return (
       <td key={Math.random()} className="row_body_cell">
-        {get(item, id, void 0)}
+        {id !== "date"
+          ? get(item, id, void 0)
+          : moment(get(item, id, void 0)).format("YYYY/ MM/ DD ")}
       </td>
     );
   }
