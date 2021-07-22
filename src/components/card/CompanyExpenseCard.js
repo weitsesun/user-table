6ef22d110c4f441266626960e749fd9030a4751d
Wei-Tse Sun <wtse.sun@gmail.com>
2021-07-21 17:16:14 -0700

    add company expense table

--- src/components/card/CompanyExpenseCard.js
+++ src/components/card/CompanyExpenseCard.js
@@ -6,13 +6,33 @@ export default function CompanyExpenseCard({ data = [] }) {
 
   // parseData to => [{category: "food", cost: 123}, {category: "health", cost: 123...}]
   useEffect(() => {
-    
+    const category = ["food", "travel", "health", "supplies"];
+    const TotalData = { food: 0, travel: 0, health: 0, supplies: 0 };
+
+    data.forEach((user) => {
+      category.forEach((category) => {
+        let costOnCategory = user.category[category].reduce((acc, record) => {
+          return (acc += Number(record.cost));
+        }, 0);
+        TotalData[category] =
+          Number(TotalData[category]) + Number(costOnCategory);
+      });
+    });
+
+    const newCompanyExpenseData = Object.entries(TotalData).map(
+      ([key, value]) => ({
+        category: key,
+        cost: value
+      })
+    );
+    console.log(newCompanyExpenseData);
+    setCompanyExpenseData(newCompanyExpenseData);
   }, [data]);
 
   return (
     <div className="card">
       <h1 className="title">Company Expense</h1>
-      <CompanyExpenseTable companyExpenseData={companyExpenseData}/>
+      <CompanyExpenseTable companyExpenseData={companyExpenseData} />
     </div>
   );
 }
